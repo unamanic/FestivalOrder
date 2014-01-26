a3d0e3d26afd81181847d9d150eff3fd3b3baccd
William Witt <william@witt-family.net>
2014-01-26 12:51:14 -0600

    Initial Commit

---
+++ FestivalOrders/BOAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  BOAppDelegate.h
+//  FestivalOrders
+//
+//  Created by William Witt on 1/26/14.
+//  Copyright (c) 2014 William Witt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BOAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ FestivalOrders/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  FestivalOrders
+//
+//  Created by William Witt on 1/26/14.
+//  Copyright (c) 2014 William Witt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
